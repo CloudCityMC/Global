3ab88d5b09a1bf313de4698b43651a3052362145
Chewsterchew <chew@chew.pw>
2018-02-18 08:46:14 -0600

    Apperantly an extra slash means error 404. Let's fix that

--- redirects.js
+++ redirects.js
@@ -12,17 +12,17 @@ if (href == "/shop") {
   window.location = "https://forums.cloudcitymc.us/threads/items-that-should-be-sold-in-the-shop.10/";
 }
 if (href == "/rules") {
-  window.location = "https://smp.cloudcitymc.us/rules/";
+  window.location = "https://smp.cloudcitymc.us/rules";
 }
 if (href == "/terms") {
-  window.location = "https://smp.cloudcitymc.us/terms/";
+  window.location = "https://smp.cloudcitymc.us/terms";
 }
 if (href == "/UsefulCommands") {
-  window.location = "https://smp.cloudcitymc.us/UsefulCommands/";
+  window.location = "https://smp.cloudcitymc.us/UsefulCommands";
 }
 if (href == "/ItemPrices") {
-  window.location = "https://smp.cloudcitymc.us/ItemPrices/";
+  window.location = "https://smp.cloudcitymc.us/ItemPrices";
 }
 if (href == "/enchantments") {
-  window.location = "https://smp.cloudcitymc.us/enchantments/";
+  window.location = "https://smp.cloudcitymc.us/enchantments";
 }
